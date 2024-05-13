d4cc9bfc34e08f1d2121bfaa0dd35cdd36a74a3f
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-13 22:28:14 +0430

    adding  html code for the input message

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // This code use for for addEventListener by click
-let textClick= document.querySelector('p')
+// let textClick= document.querySelector('p')
 
-function changeText(){
-    textClick.textContent = "Clicked"
-    textClick.style.color = "red"
-}
-textClick.addEventListener('click', changeText)
+// function changeText(){
+//     textClick.textContent = "Clicked"
+//     textClick.style.color = "red"
+// }
+// textClick.addEventListener('click', changeText)
+
