7f495e2a53d5eda82d4992f588343d9ddc50734c
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-13 15:02:47 +0430

    some comment

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+// This code use for for addEventListener by click
 let textClick= document.querySelector('p')
 
 function changeText(){
     textClick.textContent = "Clicked"
     textClick.style.color = "red"
 }
-
 textClick.addEventListener('click', changeText)
