499d10c8e60fd2827ddb0ef8b074f14d6bb0aced
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-13 23:02:40 +0430

    allowing the max length implemented by JS

--- script.js
+++ script.js
@@ -10,7 +10,21 @@
 
 let userMessage = document.getElementById('user-message');
 let remainText = document.getElementById('remaining-texts');
+let maxAllowedText = userMessage.maxLength; 
 
 function updateRemainingText(event){
-    
+    let enteredText = event.target.value;
+    let enteredTextLength = enteredText.length;
+    let remainTextLength = maxAllowedText - enteredTextLength;
+    remainText.textContent = remainTextLength;
+
+    // let textLength = userMessage.value.length;
+    // remainText.textContent = 70 - textLength;
+    // if(textLength > 70){
+    //     remainText.style.color ='red';
+    // }else{
+    //     remainText.style.color = 'black';
+    // }   
 }
+
+userMessage.addEventListener('input', updateRemainingText);
