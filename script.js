125aae30437434202a2f68cb3fa2804c06729e31
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-17 22:23:02 +0430

    comments the prevouse practice

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-// This code use for for addEventListener by click
-// let textClick= document.querySelector('p')
 
-// function changeText(){
-//     textClick.textContent = "Clicked"
-//     textClick.style.color = "red"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JS Code for counting number of remaining characters
+
+// let userInput = document.getElementById('user-input');
+// let remainInput = document.getElementById('remaining-inputs');
+// let maxAllowedText = userInput.maxLength; 
+
+// function updateRemainingInput(){
+//     let inputLength = userInput.value.length;
+//     let remainInputLength = maxAllowedText - inputLength;
+//     remainInput.textContent = remainInputLength;
+
+//     if(inputLength >=  50){
+//         remainInput.style.color ='red';
+//     }else{
+//         remainInput.style.color = 'black';
+//     }
 // }
-// textClick.addEventListener('click', changeText)
-
-
-let userInput = document.getElementById('user-input');
-let remainInput = document.getElementById('remaining-inputs');
-let maxAllowedText = userInput.maxLength; 
-
-function updateRemainingInput(){
-    let inputLength = userInput.value.length;
-    let remainInputLength = maxAllowedText - inputLength;
-    remainInput.textContent = remainInputLength;
-
-    if(inputLength >=  50){
-        remainInput.style.color ='red';
-    }else{
-        remainInput.style.color = 'black';
-    }
-}
-userInput.addEventListener('input', updateRemainingInput);
+// userInput.addEventListener('input', updateRemainingInput);
