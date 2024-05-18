2f10b174bd12ef5802cea7e6f326286d42e1a2d3
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-18 13:33:07 +0430

    creating new js file for all practices

---
+++ general.js
@@ -0,0 +1,18 @@
+// JS Code for counting number of remaining characters
+
+let userInput = document.getElementById('user-input');
+let remainInput = document.getElementById('remaining-inputs');
+let maxAllowedText = userInput.maxLength; 
+
+function updateRemainingInput(){
+    let inputLength = userInput.value.length;
+    let remainInputLength = maxAllowedText - inputLength;
+    remainInput.textContent = remainInputLength;
+
+    if(inputLength >=  50){
+        remainInput.style.color ='red';
+    }else{
+        remainInput.style.color = 'black';
+    }
+}
+userInput.addEventListener('input', updateRemainingInput);
