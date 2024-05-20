4dcedec91be2ea8e94b247ff2eb505b240dd25b3
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-20 12:03:53 +0430

    calculate the sum of entered number completed

--- General Practices/general.js
+++ General Practices/general.js
@@ -1,5 +1,4 @@
 // JS Code for counting number of remaining characters
-
 let userInput = document.getElementById('user-input');
 let remainInput = document.getElementById('remaining-inputs');
 let maxAllowedText = userInput.maxLength;
@@ -16,3 +15,22 @@ function updateRemainingInput(){
     }
 }
 userInput.addEventListener('input', updateRemainingInput);
+
+
+
+// JS code for calculating the sum of entered number by user 
+let calculateSumButtonElement = document.querySelector('#calculator button');
+
+function calculateSum(){
+    let user_number= document.getElementById('user-number');
+    let enteredNumber= user_number.value 
+    let sumTotal=0;
+    for (i= 0; i<=enteredNumber; i++){
+        sumTotal = sumTotal + i;
+    }
+    let outputResult = document.getElementById('calculated-sum');
+    outputResult.textContent = sumTotal;
+    outputResult.style.display = 'block';
+}
+
+calculateSumButtonElement.addEventListener('click', calculateSum)
