b395d16bd7dcbc6eef2676c261dd533cbaa9dd80
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-23 12:00:41 +0430

    close and open add event added for user

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const user1_adding_btn = document.getElementById('edit-player-1-btn');
 const user2_adding_btn = document.getElementById('edit-player-2-btn');
-// const playerConfigModel = document.querySelector('.model');
+
+const playerConfigOverlay = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
+const overlay_cancel = document.getElementById('cancel-config-btn'); 
 
 function OpenPlayerConfig(){
-    // playerConfigModel.classList.remove('display-none');
+    playerConfigOverlay.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function ClosePayerConfig(){
+    playerConfigOverlay.style.display = 'none';
+    backdropElement.style.display = 'none';
 }
 
 user1_adding_btn.addEventListener('click', OpenPlayerConfig);
 user2_adding_btn.addEventListener('click', OpenPlayerConfig);
 
+
+overlay_cancel.addEventListener('click', ClosePayerConfig);
+backdropElement.addEventListener('click', ClosePayerConfig) 
