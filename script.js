c180af0cbdfede81202d21790164d9f858619b91
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-23 15:54:12 +0430

    form and uer input validation added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const user1_adding_btn = document.getElementById('edit-player-1-btn');
 const user2_adding_btn = document.getElementById('edit-player-2-btn');
 const formElement= document.querySelector('form');
+const errorOutputElement= document.getElementById('config-errors');
 
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
@@ -30,3 +31,17 @@ backdropElement.addEventListener('click', ClosePayerConfig);
 
 
 // JS code for getting the user name from user config and display it
+///////////////////////////////////////////////////////////////////
+
+function savePlayerConfig(event){
+     event.preventDefault();
+    const formData = new FormData(event.target);
+    const playerName = formData.get('playerName').trim(); // term(); is use to remove the extra spaces like '  ' will consider as '' and '  M ' as 'M'
+    if (!playerName){ // we can add the condition like this to payerName === '' 
+        errorOutputElement.textContent = 'Please enter a valid player name';
+        return;
+    }
+    console.log(playerName);
+}
+
+formElement.addEventListener('submit', savePlayerConfig);
