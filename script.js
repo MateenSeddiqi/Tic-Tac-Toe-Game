6777ad13900f5ffcd16adb411c4bc19d2f09465d
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-23 14:59:27 +0430

    organizing JS code with commands

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+// JS variables for all IDs and elements
+//////////////////////////////// 
 const user1_adding_btn = document.getElementById('edit-player-1-btn');
 const user2_adding_btn = document.getElementById('edit-player-2-btn');
+const formElement= document.querySelector('form');
 
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const overlay_cancel = document.getElementById('cancel-config-btn'); 
 
+
+
+// JS code for opening and closing the config overlay for adding the user name
+//////////////////////////////////////////////////////////////// 
 function OpenPlayerConfig(){
     playerConfigOverlay.style.display = 'block';
     backdropElement.style.display = 'block';
@@ -18,6 +25,8 @@ function ClosePayerConfig(){
 user1_adding_btn.addEventListener('click', OpenPlayerConfig);
 user2_adding_btn.addEventListener('click', OpenPlayerConfig);
 
-
 overlay_cancel.addEventListener('click', ClosePayerConfig);
-backdropElement.addEventListener('click', ClosePayerConfig) 
+backdropElement.addEventListener('click', ClosePayerConfig);
+
+
+// JS code for getting the user name from user config and display it
