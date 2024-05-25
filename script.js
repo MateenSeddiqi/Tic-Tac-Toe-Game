cd1de1c687439dfb17ac928443e9137084d426aa
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-05-25 11:03:30 +0430

    displaying the name of player and store them

--- script.js
+++ script.js
@@ -4,19 +4,28 @@ const user1_adding_btn = document.getElementById('edit-player-1-btn');
 const user2_adding_btn = document.getElementById('edit-player-2-btn');
 const formElement= document.querySelector('form');
 const errorOutputElement= document.getElementById('config-errors');
-let editedPlayer = 0;
-
+const playerNameInput = document.getElementById('playername');
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const overlay_cancel = document.getElementById('cancel-config-btn'); 
 
+let editedPlayer = 0;
+const players = [ // this code is use to store the name and symbol of the player
+    {
+        name: '',
+        Symbol: 'X'
+    },
+    {
+        name: '',
+        Symbol: 'O'
+    },
+];
 
 
 // JS code for opening and closing the config overlay for adding the user name
 //////////////////////////////////////////////////////////////// 
 function OpenPlayerConfig(event){
-    editedPlayer = +event.target.dataset.playerid; // +'1' = > 1
-    console.log('ID '+editedPlayer);
+    editedPlayer = +event.target.dataset.playerid; // this code use to take the player ID when user click on edit btn it mean:  +'1' = > 1
     playerConfigOverlay.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -26,8 +35,10 @@ function ClosePayerConfig(){
     backdropElement.style.display = 'none';
     formElement.firstElementChild.classList.remove('error');
     errorOutputElement.textContent = '';
-}
+    playerNameInput.value = '';
 
+}
+// Add EvntListener for both player name btn and closing the overlay and backdrop
 user1_adding_btn.addEventListener('click', OpenPlayerConfig);
 user2_adding_btn.addEventListener('click', OpenPlayerConfig);
 
@@ -35,20 +46,26 @@ overlay_cancel.addEventListener('click', ClosePayerConfig);
 backdropElement.addEventListener('click', ClosePayerConfig);
 
 
-// JS code for getting the user name from user config and display it
+// JS code for getting the user name from user config and display it in the player name field
 ///////////////////////////////////////////////////////////////////
 
 function savePlayerConfig(event){
     event.preventDefault();
     const formData = new FormData(event.target);
-    const playerName = formData.get('playerName').trim(); // term(); is use to remove the extra spaces like '  ' will consider as '' and '  M ' as 'M'
-    if (!playerName){ // we can add the condition like this to payerName === '' 
+    const enteredPlayerName = formData.get('playerName').trim(); // term(); is use to remove the extra spaces like '  ' will consider as '' and '  M ' as 'M'
+    if (!enteredPlayerName){ // we can add the condition like this to payerName === '' 
         event.target.firstElementChild.classList.add('error');
         errorOutputElement.textContent = 'Please enter a valid player name';
         return;
     }
     const dataPlayerDataElement = document.getElementById('player-'+ editedPlayer + '-data');
-    console.log(dataPlayerDataElement);
-    dataPlayerDataElement.children[1].textContent = playerName;
+    dataPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+    if (editedPlayer === 1 ){ // This if first check the player ID and store it in player array 
+        players[0]=enteredPlayerName;
+    }else {
+        players[1]=enteredPlayerName;
+    }
+    ClosePayerConfig();
 }
 formElement.addEventListener('submit', savePlayerConfig);
